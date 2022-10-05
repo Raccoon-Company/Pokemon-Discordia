b1824204ce962ab9b691d79ac13325b961a73d4f
Olivier Tafforin <olivier.tafforin@gmail.com>
2022-10-05 14:57:37 +0200

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.Compression;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import javax.annotation.Nonnull;
+
+public class Main extends ListenerAdapter {
+    public static void main(String[] args){
+        JDABuilder builder = JDABuilder.createDefault(args[0]);
+
+        // Disable parts of the cache
+        builder.disableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE);
+        // Enable the bulk delete event
+        builder.setBulkDeleteSplittingEnabled(false);
+        // Disable compression (not recommended)
+        builder.setCompression(Compression.NONE);
+        // Set activity (like "playing Something")
+        builder.setActivity(Activity.playing("!help"));
+
+        builder.addEventListeners(new Main());
+
+        builder.enableIntents(
+                GatewayIntent.GUILD_MESSAGES,
+                GatewayIntent.MESSAGE_CONTENT,
+                GatewayIntent.GUILD_MESSAGE_REACTIONS,
+                GatewayIntent.GUILD_MEMBERS);
+
+        builder.build();
+    }
+
+    @Override
+    public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
+        if(event.getAuthor().isBot()){
+            return;
+        }
+        System.out.println("Message reçu : "+event.getAuthor().getName()+", il dit : "+event.getMessage().getContentDisplay());
+        event.getMessage().getChannel().sendMessage("ca marche !").complete();
+    }
+}
