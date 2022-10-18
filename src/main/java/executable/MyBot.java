e919e0734c9369c3764fb43122f9593dd8f49e2a
O_TAFFORIN <olivier.tafforin@mutuelledepoitiers.fr>
2022-10-18 16:46:03 +0200

    merge image procedure

--- src/main/java/executable/MyBot.java
+++ src/main/java/executable/MyBot.java
@@ -18,11 +18,53 @@ import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class MyBot {
+
+//    File path = new File("src/main/resources/images"); // base path of the images
+//
+//    // load source images
+//    BufferedImage background = null;
+//    BufferedImage overlay = null;
+//    BufferedImage overlay2 = null;
+//        try {
+//        background = ImageIO.read(new File(path, "bg.jpg"));
+//        overlay = ImageIO.read(new File(path, "132.png"));
+//        overlay2 = ImageIO.read(new File(path, "25.png"));
+//    } catch (
+//    IOException e) {
+//        throw new RuntimeException(e);
+//    }
+//    // create the new image, canvas size is the max. of both image sizes
+////        int w = Math.max(background.getWidth(), overlay.getWidth());
+////        int h = Math.max(background.getHeight(), overlay.getHeight());
+//    int w = 200;
+//    int h = 120;
+//    BufferedImage combined = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+//
+//    // paint both images, preserving the alpha channels
+//    Graphics g = combined.getGraphics();
+//        g.drawImage(background, 0, 0, null);
+//        g.drawImage(overlay, 0, 60, null);
+//        g.drawImage(overlay2, 100, 0, null);
+//
+//        g.dispose();
+//
+//// Save as new image
+//        try {
+//        ImageIO.write(combined, "PNG", new File(path, "combined.png"));
+//    } catch (IOException e) {
+//        throw new RuntimeException(e);
+//    }
+//}
 
     private final EventWaiter eventWaiter = new EventWaiter();
     private final Logger logger = LoggerFactory.getLogger(MyBot.class);
